e4a374441bb7d9ab5f807abdf9fa7c696c3eb3c6
AkshitTyagi7 <102504167+AkshitTyagi7@users.noreply.github.com>
2022-08-03 17:09:13 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = ["https://robertogo.000webhostapp.com/"]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
